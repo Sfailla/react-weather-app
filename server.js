768e9629ce5ee97237b0ba07e7e3bedf78b0542e
Stevo99 <Sfailla1983@gmail.com>
2019-07-20 13:13:20 -0400

    change the request to https

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var CLIENT_DIR = path.join(__dirname, 'client');
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-//Serving the files on the CLIENT folder
-app.use(express.static(CLIENT_DIR));
+if (process.env.NODE_ENV === 'production') {
+	//Serving the files on the CLIENT folder
+	app.use(express.static(CLIENT_DIR));
+
+	app.use((req, res, next) => {
+		if (req.header('x-forwarded-proto') !== 'https') {
+			res.redirect(`https://${req.header('host')}${req.url}`);
+		} else {
+			next();
+		}
+	});
+}
 
 //Send index.html when the user access the web
 app.get('*', function(req, res) {
