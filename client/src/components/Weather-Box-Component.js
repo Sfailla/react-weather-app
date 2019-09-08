c5bf6067d38ef3dea8a5d270a5944b698bc6cbc5
Stevo99 <Sfailla1983@gmail.com>
2019-09-08 14:16:00 -0400

    fixed geolocation method

--- client/src/components/Weather-Box-Component.js
+++ client/src/components/Weather-Box-Component.js
@@ -16,29 +16,60 @@ export default class MainBoxComponent extends Component {
 
 	handleUnixToDayOfWeek = timeStamp => {
 		let date = new Date(timeStamp * 1000);
-		const days = [ 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' ];
+		const days = [
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+			'Sunday'
+		];
 		const dayOfWeek = days[date.getDay()];
 		return dayOfWeek;
 	};
 
 	handleGetData = () => {
 		const { url } = this.state;
-		fetch(url).then(res => res.json()).then(data => data).catch(error => console.log(error));
+		fetch(url)
+			.then(res => res.json())
+			.then(data => data)
+			.catch(error => console.log(error));
+	};
+
+	handleReverseGeoLocate = (lat, lon) => {
+		let key = 'AIzaSyAE3y9x37WZUZrzkhq9rXJF76lrVBpvMqA';
+		let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=${key}`;
+		fetch(url).then(res => res.json()).then(data => {
+			let area = data.results[0].formatted_address;
+			let splitArea = area.split(',');
+			this.setState({ location: splitArea[1], region: splitArea[2] });
+		});
 	};
 
 	handleGeoLocate = () => {
 		navigator.geolocation.getCurrentPosition(position => {
 			this.setState(() => ({
-				url: `https://api.apixu.com/v1/forecast.json?key=${api.key}&days=5&q=${position.coords
-					.latitude},${position.coords.longitude}`
+				url: `https://api.apixu.com/v1/forecast.json?key=${api.key}&days=5&q=${position
+					.coords.latitude},${position.coords.longitude}`,
+				lat: position.coords.latitude,
+				lon: position.coords.longitude
 			}));
 			fetch(this.state.url)
 				.then(res => res.json())
 				.then(data => {
+					console.log(data);
 					let weather = data.forecast.forecastday;
-					let location = data.location.name;
+					let location =
+						data.location.name !== ''
+							? data.location.name
+							: this.handleReverseGeoLocate(
+									this.state.lat,
+									this.state.lon
+								);
 					let region =
-						data.location.region === '' || data.location.region === data.location.name
+						data.location.region === '' ||
+						data.location.region === data.location.name
 							? data.location.country
 							: data.location.region;
 					this.setState(() => ({ weather, location, region }));
@@ -63,7 +94,8 @@ export default class MainBoxComponent extends Component {
 					let weather = data.forecast.forecastday;
 					let location = data.location.name;
 					let region =
-						data.location.region === '' || data.location.region === data.location.name
+						data.location.region === '' ||
+						data.location.region === data.location.name
 							? data.location.country
 							: data.location.region;
 					this.setState(() => ({ weather, location, region }));
@@ -73,13 +105,12 @@ export default class MainBoxComponent extends Component {
 	};
 
 	componentDidMount = () => {
-		this.handleGetData();
 		this.handleGeoLocate();
+		this.handleGetData();
 	};
 
 	render() {
-		const { weather, location, region, url } = this.state;
-		const { value } = this.props;
+		const { weather, location, region } = this.state;
 		let dailyForecast =
 			Array.isArray(weather) && weather.length ? (
 				weather.map((data, index) => {
@@ -94,7 +125,9 @@ export default class MainBoxComponent extends Component {
 					);
 				})
 			) : (
-				<h1 className="weather-box__loading">Loading Weather Data...</h1>
+				<h1 className="weather-box__loading">
+					Loading Weather Data...
+				</h1>
 			);
 
 		return (
@@ -103,7 +136,9 @@ export default class MainBoxComponent extends Component {
 					<h1 className="weather-box__location">
 						{location}, {region}
 					</h1>
-					<div className="weather-box__container container">{dailyForecast}</div>
+					<div className="weather-box__container container">
+						{dailyForecast}
+					</div>
 				</div>
 				<InputComponent onUpdate={this.handleOnUpdate} />
 			</div>
--- server.js
+++ server.js
@@ -18,6 +18,8 @@ if (process.env.NODE_ENV === 'production') {
 	});
 }
 
+console.log(__dirname);
+
 //Send index.html when the user access the web
 app.get('*', function(req, res) {
 	res.sendFile(path.join(CLIENT_DIR, 'index.html'));
