48f1ff148d47424db6cfa9a94835bdb6d0cf8b46
Stevo99 <Sfailla1983@gmail.com>
2019-11-24 15:49:58 -0500

    minor changes to server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var path = require('path');
 var express = require('express');
+var cors = require('cors');
 
 var CLIENT_DIR = path.join(__dirname, 'client');
 var PORT = process.env.PORT || 3000;
 var app = express();
 
+app.use(cors());
+
 if (process.env.NODE_ENV === 'production') {
 	//Serving the files on the CLIENT folder
 	app.use(express.static(CLIENT_DIR));
-
+	app.use(cors());
 	app.use((req, res, next) => {
 		if (req.header('x-forwarded-proto') !== 'https') {
 			res.redirect(`https://${req.header('host')}${req.url}`);
