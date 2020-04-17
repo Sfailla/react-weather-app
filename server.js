b0b404fae30a3a63bb5a92aecb2c103ea33ff08f
Stevo99 <Sfailla1983@gmail.com>
2020-04-17 09:59:34 -0400

    added comment to server file

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.static(CLIENT_DIR));
 if (process.env.NODE_ENV === 'production') {
 	//Serving the files on the CLIENT folder
 	app.use((req, res, next) => {
+		// if not an https server, the site will be redirected to https
 		if (req.header('x-forwarded-proto') !== 'https') {
 			res.redirect(`https://${req.header('host')}${req.url}`);
 		} else {
