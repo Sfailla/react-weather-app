ff7419c36bbe0f447b21be4c73f74515d0a68362
Sfailla <Sfailla1983@gmail.com>
2021-01-04 16:18:53 -0500

    updated app

--- client/src/app.js
+++ client/src/app.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 
 import './styles/style.scss';
 
-import App from './components/App-Component';
-
+import App from './components/Main';
 
 ReactDOM.render(<App />, document.getElementById('app'));
